33cb76ee86aa46bfd0f22c37ee95dbe3c4f048ac
eusouregislima <danilolima.cogic@gmail.com>
2022-09-27 22:31:23 -0300

    first commit

---
+++ src/database/migrations/20220829192943-create-users.js
@@ -0,0 +1,46 @@
+"use strict"
+
+const sequelize = require("sequelize")
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: sequelize.STRING,
+        allowNull: false,
+      },
+      admin: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    })
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable("users")
+  },
+}
